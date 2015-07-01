ea05918a54a6a65e7cc27c60fafe2eb90ee1ce3a
TimePath <andrew.hardaker1995@gmail.com>
2015-07-01 14:21:24 +1000

    Handle missing game data

--- src/test/kotlin/com/timepath/steam/io/VDFTest.kt
+++ src/test/kotlin/com/timepath/steam/io/VDFTest.kt
@@ -5,58 +5,58 @@ import org.junit.After
 import org.junit.Assert.fail
 import org.junit.Before
 import org.junit.Test
-import java.io.IOException
+import java.io.FileNotFoundException
 import java.io.PrintStream
 import java.nio.charset.Charset
 import java.util.LinkedList
 
 public class VDFTest {
 
-    Before
-    throws(Exception::class)
+    @Before
     public fun setUp() {
     }
 
-    After
-    throws(Exception::class)
+    @After
     public fun tearDown() {
     }
 
-    Test
-    throws(Exception::class)
+    @Test
     public fun testLoad() {
-        val max = 10
-        val a = ACF.fromManifest(440)
-        val original = System.err
-        val warnings = LinkedList<String>()
-        for (s in listOf(".res", ".vdf")) {
-            for (f in a.find(s)) {
-                var flag = false
-                System.setErr(object : PrintStream(original) {
-                    override fun write(b: ByteArray) {
-                        super.write(b)
-                        flag = true
-                    }
+        try {
+            val max = 10
+            val a = ACF.fromManifest(440)
+            val original = System.err
+            val warnings = LinkedList<String>()
+            for (s in listOf(".res", ".vdf")) {
+                for (f in a.find(s)) {
+                    var flag = false
+                    System.setErr(object : PrintStream(original) {
+                        override fun write(b: ByteArray) {
+                            super.write(b)
+                            flag = true
+                        }
 
-                    override fun write(buf: ByteArray, off: Int, len: Int) {
-                        super.write(buf, off, len)
-                        flag = true
-                    }
+                        override fun write(buf: ByteArray, off: Int, len: Int) {
+                            super.write(buf, off, len)
+                            flag = true
+                        }
 
-                    override fun write(b: Int) {
-                        super.write(b)
-                        flag = true
+                        override fun write(b: Int) {
+                            super.write(b)
+                            flag = true
+                        }
+                    })
+                    VDF.load(f.openStream()!!, Charset.defaultCharset())
+                    if (flag) {
+                        warnings.add(f.toString())
+                        System.err.println("Warning from: ${f.path}${f.name}")
                     }
-                })
-                VDF.load(f.openStream()!!, Charset.defaultCharset())
-                if (flag) {
-                    warnings.add(f.toString())
-                    System.err.println("Warning from: ${f.path}${f.name}")
-                }
-                if (warnings.size() > max) {
-                    fail("Too many warnings: $warnings")
+                    if (warnings.size() > max) {
+                        fail("Too many warnings: $warnings")
+                    }
                 }
             }
+        } catch(e: FileNotFoundException) {
         }
     }
 }
